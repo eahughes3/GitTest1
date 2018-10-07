fe1c45ded9a40d17b30cc0a18306379963255fcf
eahughes3 <eahughes3@users.noreply.github.com>
2018-10-07 00:19:38 -0500

    Update Feature1.cs
    Changed int to var

--- ConsoleApp1/ConsoleApp1/Feature1.cs
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
     {
         public int Add()
         {
+            //Changed int to var
             var x1 = 1;
             var x2 = 2;
             var sum = x1 + x2;
